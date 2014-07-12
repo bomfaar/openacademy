24198522247288434af11137bbb30992e3e93932
Gabriel Schwanen <gabriel.schwanen@fundp.ac.be>
2014-07-12 20:04:55 +0200

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name":"Open Academy",
+    "depends":["base"],
+    "author":"Gabriel Schwanen",
+    "data":[
+        "openacademy_view.xml",
+        "partner_view.xml",
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "wizard/create_attendee_view.xml",
+    ],
+    "installable":True,
+    "application":True,
+    "active":False,
+}
